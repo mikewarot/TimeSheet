9a014bda778e88233958ff40c7e4f0ee82faf405
Mike Warot <mike@warot.com>
2014-02-27 11:55:54 -0600

    Now can update phone numbers properly

--- TimeSheet/TimeSheet/Form1.vb
+++ TimeSheet/TimeSheet/Form1.vb
@@ -8,4 +8,20 @@
         Application.Exit()
 
     End Sub
+
+    Private Sub BindingSource1_CurrentChanged(sender As Object, e As EventArgs) Handles BindingSource1.CurrentChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'TimeSheetDataSet.Users' table. You can move, or remove it, as needed.
+        Me.UsersTableAdapter.Fill(Me.TimeSheetDataSet.Users)
+        'TODO: This line of code loads data into the 'TimeSheetDataSet.Users' table. You can move, or remove it, as needed.
+        Me.UsersTableAdapter.Fill(Me.TimeSheetDataSet.Users)
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
 End Class
