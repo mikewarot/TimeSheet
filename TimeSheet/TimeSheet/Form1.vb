fef6e5e2142409fd528bc593a77430ff9c0c7a69
Mike Warot <mike@warot.com>
2014-02-27 10:32:59 -0600

    Added code to exit the program

--- TimeSheet/TimeSheet/Form1.vb
+++ TimeSheet/TimeSheet/Form1.vb
@@ -3,4 +3,9 @@
     Private Sub AboutThisProgramToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutThisProgramToolStripMenuItem.Click
         MessageBox.Show("This is a test")
     End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+
+    End Sub
 End Class
